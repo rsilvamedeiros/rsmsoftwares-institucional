222b0af878f876db0cbfbe7c3de40a6edeed1ad7
Renan Medeiros <renanmedeiros79@gmail.com>
2025-01-14 22:43:15 -0300

    testes no nextJS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,6 +22,8 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
+        
+        <h1>TESTEEE</h1>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
